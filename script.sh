e8d49fb0f77f2f955a1182c14b8b962188bac8f7
Atte902 <atte902@gmail.com>
2019-02-09 17:08:30 +0200

    adding new line between

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 snap install docker
-docker run -d --name chat -e ASPNETCORE_ENVIRONMENT='Development' -p 51069:80 atte902/chatwebapp:testi
 
+docker run -d --name chat -e ASPNETCORE_ENVIRONMENT='Development' -p 51069:80 atte902/chatwebapp:testi
