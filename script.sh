1c8be3b4dd7106626fbad30c6646f26720f0d146
Atte902 <atte902@gmail.com>
2019-02-09 14:57:04 +0200

    removed sleep and pull

--- script.sh
+++ script.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 snap install docker
-sleep 20s
-docker pull docker.io/atte902/chatwebapp:testi
 docker run -d --name chat -e ASPNETCORE_ENVIRONMENT='Development' -p 51069:80 atte902/chatwebapp:testi
