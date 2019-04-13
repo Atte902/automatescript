f53e8f75836c59414dd1d956e129f954b06814a9
Atte902 <atte902@gmail.com>
2019-04-13 14:15:20 +0300

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
-apt update
-apt install -y docker.io
-docker run -d --name chat -e ASPNETCORE_ENVIRONMENT='Development' -p 51069:80 atte902/chatwebapp:testi
+#!/bin/sh
+if [ `sudo systemctl is-active docker` != "active" ]
+then
+	apt update
+	apt install -y docker.io
+fi
+if [ $(docker inspect -f '{{.State.Running}}' chat) = "false" ]
+then
+	docker start chat
+elif [ $(docker inspect -f '{{.State.Running}}' chat) = "true" ]
+then
+	echo "Already running"
+else
+	docker run -d --name chat -e ASPNETCORE_ENVIRONMENT='Development' -p 51069:80 --restart always atte902/chatwebapp:testi 
+fi
